0747aa20fc65900507c5cc702f42e78197fc4577
Alexey Zimarev <alex@zimarev.com>
2018-01-31 00:15:54 +0100

    Adding one before

---
+++ exercises/01-the-basics/before/src/Marketplace.Domain/ClassifiedAds/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Marketplace.Domain.ClassifiedAds
+{
+    public static class Events
+    {
+        public static class V1
+        {
+            public class ClassifiedAdCreated
+            {
+                public Guid Id { get; set; }
+                public Guid Owner { get; set; }
+                public string Title { get; set; }
+                public DateTimeOffset CreatedAt { get; set; }
+                public Guid CreatedBy { get; set; }
+            }
+        }
+    }
+}
