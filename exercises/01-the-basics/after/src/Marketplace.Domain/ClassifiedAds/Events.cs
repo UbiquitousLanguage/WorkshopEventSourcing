25ad7ec1937f24ee36a361c21cee63c09929f54a
Alexey Zimarev <alex@zimarev.com>
2018-01-31 00:09:54 +0100

    Adding one after

---
+++ exercises/01-the-basics/after/src/Marketplace.Domain/ClassifiedAds/Events.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Marketplace.Domain.ClassifiedAds
+{
+    public static class Events
+    {
+        public static class V1
+        {
+            public class ClassifiedAdCreated
+            {
+                public Guid Id { get; set; }
+                public Guid Owner { get; set; }
+                public string Title { get; set; }
+                public DateTimeOffset CreatedAt { get; set; }
+                public Guid CreatedBy { get; set; }
+            }
+
+            public class ClassifiedAdRenamed
+            {
+                public Guid Id { get; set; }
+                public Title Title { get; set; }
+                public DateTimeOffset RenamedAt { get; set; }
+                public Guid RenamedBy { get; set; }
+            }
+            
+            public class ClassifiedAdTextUpdated
+            {
+                public Guid Id { get; set; }
+                public string AdText { get; set; }
+                public DateTimeOffset TextUpdatedAt { get; set; }
+                public Guid TextUpdatedBy { get; set; }
+            }
+
+            public class ClassifiedAdPriceChanged
+            {
+                public Guid Id { get; set; }
+                public double Price { get; set; }
+                public DateTimeOffset PriceChangedAt { get; set; }
+                public Guid PriceChangedBy { get; set; }
+            }
+        }
+    }
+}
---
+++ exercises/01-the-basics/after/src/Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -0,0 +1,15 @@
+﻿using Marketplace.Contracts;
+using Marketplace.Domain.ClassifiedAds;
+
+namespace Marketplace
+{
+    public class ClassifiedAdsApplicationService
+    {
+        public void Handle(ClassifiedAds.V1.Create command) =>
+            ClassifiedAd.Create(
+                command.Id,
+                command.OwnerId,
+                command.CreatedBy,
+                command.CreatedAt);
+    }
+}
