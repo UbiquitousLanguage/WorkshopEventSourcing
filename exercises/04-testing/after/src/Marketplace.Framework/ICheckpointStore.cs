6ad41eaf35d68396ad31103c48e162b5545fa6f3
Alexey Zimarev <alex@zimarev.com>
2018-01-30 23:29:55 +0100

    Adding four after

---
+++ exercises/04-testing/after/src/Marketplace.Framework/ICheckpointStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Marketplace.Framework
+{
+    public interface ICheckpointStore
+    {
+        Task<T> GetLastCheckpoint<T>(string projection);
+        Task SetCheckpoint<T>(T checkpoint, string projection);
+    }
+}
