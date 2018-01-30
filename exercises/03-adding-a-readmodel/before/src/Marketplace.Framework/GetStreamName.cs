c7b7ff81540bd6c2c47c6640936500e584bae774
Alexey Zimarev <alex@zimarev.com>
2018-01-30 23:39:09 +0100

    Adding three before

---
+++ exercises/03-adding-a-readmodel/before/src/Marketplace.Framework/GetStreamName.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace Marketplace.Framework
+{
+    public delegate string GetStreamName(Type aggregateType, string aggregateId);
+}
---
+++ exercises/03-adding-a-readmodel/before/src/Marketplace.Framework/WrongExpectedStreamVersionException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marketplace.Framework
+{
+    public class WrongExpectedStreamVersionException : Exception
+    {
+        public WrongExpectedStreamVersionException(string message, Exception innerException = null)
+            : base(message, innerException)
+        {
+        }
+    }
+}
