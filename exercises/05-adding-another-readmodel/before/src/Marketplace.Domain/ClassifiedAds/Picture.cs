16909d9850cdff89142dd8620376a1f254ef7d4f
Alexey Zimarev <alex@zimarev.com>
2018-01-30 23:25:19 +0100

    Adding five before

---
+++ exercises/05-adding-another-readmodel/before/src/Marketplace.Domain/ClassifiedAds/Picture.cs
@@ -0,0 +1,16 @@
+﻿using Marketplace.Framework;
+
+namespace Marketplace.Domain.ClassifiedAds
+{
+    public class Picture : Value<Picture>
+    {
+        public string Description { get; }
+        public string Url { get; }
+
+        public Picture(string url, string description)
+        {
+            Url = url;
+            Description = description;
+        }
+    }
+}
