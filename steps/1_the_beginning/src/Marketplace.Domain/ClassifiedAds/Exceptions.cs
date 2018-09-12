0f1324b5adf66613266246db5472a381dae374b0
Alexey Zimarev <alex@zimarev.com>
2018-09-12 23:25:02 +0100

    Added steps

---
+++ steps/1_the_beginning/src/Marketplace.Domain/ClassifiedAds/Exceptions.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Marketplace.Domain.ClassifiedAds
+{
+    public class Exceptions
+    {
+        public class PriceNotAllowed : Exception{ }
+
+        public class ClassifiedAdNotFoundException : Exception
+        {
+            public ClassifiedAdNotFoundException() { }
+
+            public ClassifiedAdNotFoundException(Guid id) : base($"Classified ad with id '{id}' was not found") { }
+        }
+
+        public class ClassifiedAdActivationException : Exception
+        {
+            public ClassifiedAdActivationException(string message) : base(message) { }
+        }
+
+        public class ProfanityFound : Exception{ }
+    }
+}
---
+++ steps/1_the_beginning/src/Marketplace/Contracts/ClassifiedAds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal.Networking;
+
+namespace Marketplace.Contracts
+{
+    public static class ClassifiedAds
+    {
+        public static class V1
+        {
+            public class Create
+            {
+                public Guid Id { get; set; }
+                public Guid OwnerId { get; set; }
+                public Guid CreatedBy { get; set; }
+
+                public override string ToString() => $"Creating Classified Ad {Id}";
+            }
+
+            public class RenameAd
+            {
+                public Guid Id { get; set; }
+                public string Title { get; set; }
+                public Guid RenamedBy { get; set; }
+                
+                public override string ToString() 
+                    => $"Renaming Classified Ad {Id} to '{(Title?.Length > 25 ? $"{Title.Substring(0, 22)}..." : Title )}'";
+            }
+        }
+    }
+}
